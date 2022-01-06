5605b08f96d3b35e473c0a5752ff45c254933283
hoale0507 <hoale090293@gmail.com>
2022-01-06 16:13:54 +0700

    add file

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr1 = {1,2,3};
+        int[] arr2 = {4,5,6,7};
+        int[] newArr = new int[arr1.length + arr2.length];
+        for (int i = 0; i < newArr.length; i++) {
+            if(i < arr1.length){
+                newArr[i] = arr1[i];
+            } else {
+                newArr[i] = arr2[i - arr1.length];
+            }
+            System.out.println(newArr[i]);
+        }
+
+    }
+}
